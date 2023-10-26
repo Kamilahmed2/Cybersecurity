f8e36b223cf6e2c6a4ac917e94c430c649924201
Kamilahmed2 <148788994+Kamilahmed2@users.noreply.github.com>
2023-10-26 12:45:07 +0100

    Update telnet-to-a-network-device (1).py

--- telnet-to-a-network-device (1).py
+++ telnet-to-a-network-device (1).py
@@ -18,13 +18,33 @@ telnet_connection.write(password.encode('ascii') + b'\n')
 print('17')  
 
 # Enter Privileged EXEC Mode
+telnet_connection.read_until(b'#')import telnetlib
+
+# Define Variables
+ip_address = '192.168.56.101'
+username = input('Enter a Username: ')
+password = input('Enter a Password: ')
+hostname = 'R1'
+
+# Establish Telnet Connection
+telnet_connection = telnetlib.Telnet(ip_address)
+
+
+# Login to the device
+telnet_connection.read_until(b'Username: ')
+telnet_connection.write(username.encode('ascii') + b'\n')
+telnet_connection.read_until(b'Password: ')
+telnet_connection.write(password.encode('ascii') + b'\n')
+
+
+# Enter Privileged EXEC Mode
 telnet_connection.read_until(b'#')
-print('23')  
+
 
 # Configure the device
 telnet_connection.write(b'configure terminal\n')
 telnet_connection.read_until(b'#')
-telnet_connection.write(f'hostname {new_hostname}\n'.encode('ascii'))
+telnet_connection.write(f'hostname {hostname}\n'.encode('ascii'))
 telnet_connection.read_until(b'#')
 
 # Save the configuration
@@ -32,19 +52,24 @@ telnet_connection.write(b'end\n')
 telnet_connection.read_until(b'#')
 telnet_connection.write(b'write memory\n')
 telnet_connection.read_until(b'#')
-print("35")  
+
 
 # Send a command to the remote device to output the running configuration 
 telnet_connection.write(b'show running-config\n')
 output = telnet_connection.read_until(b'end').decode('ascii')
-print("38") 
 
 # Save the output to a file
 with open('running_config.txt', 'w') as file:
     file.write(output)
 
+print("---------------------------------------------------------------")
+print("----- SUCESS! Connecting to:", ip_address)
+print("-----              Username:", username)
+print("-----              Password:", password)
+print("----- New Hostname: ", hostname)
+print("Running configuration saved to runninf_config.txt")
+
 # Close the Telnet Connection
 telnet_connection.write(b'quit\n')
-print("42")  
 
 
